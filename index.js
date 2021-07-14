6d40e8002df9fd16db13ee5ed695ea869b67efe4
Sergio Uriona Herbas <seruriher@gmail.com>
2021-07-14 15:52:48 -0400

    Add theme toggle functionality

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 setInterval(() => {
   document.getElementById('clock').innerHTML = new Date().toLocaleTimeString()
 }, 1000)
+
+const mainElement = document.getElementById('weather-app-main')
+const themeToggle = document.getElementById('theme-toggle')
+const themeLabel = document.getElementById('theme-label')
+
+themeToggle.addEventListener('change', (e) => {
+  themeLabel.innerText = e.target.checked ? 'Dark' : 'Light'
+  mainElement.className = e.target.checked ? 'dark-theme' : 'light-theme'
+})
