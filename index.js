7c6498e557296e21a351b46942a9273491f01f99
Sergio Uriona Herbas <seruriher@gmail.com>
2021-07-14 18:50:47 -0400

    Add footer

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ setInterval(() => {
 const mainElement = document.getElementById('weather-app-main')
 const themeToggle = document.getElementById('theme-toggle-input')
 const themeLabel = document.getElementById('theme-label')
+const footer = document.getElementsByTagName('footer')[0]
 
 themeToggle.addEventListener('change', (e) => {
   themeLabel.innerText = e.target.checked ? 'Dark' : 'Light'
   mainElement.className = `${e.target.checked ? 'dark' : 'light'}-theme`
+  footer.className = `${e.target.checked ? 'dark' : 'light'}-theme`
 })
