646356dd72a55a5917df959b7d1caa8b2ced72c8
Sergio Uriona Herbas <seruriher@gmail.com>
2021-07-19 19:04:56 -0400

    Add dropdown to select city

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 'use strict'
 
+const cities = [
+  {
+    "id": "3911924",
+    "name": "La Paz"
+  },
+  {
+    "id": "3919966",
+    "name": "Cochabamba"
+  },
+  {
+    "id": "3904906",
+    "name": "Santa Cruz"
+  },
+  {
+    "id": "3920177",
+    "name": "Chuquisaca"
+  },
+  {
+    "id": "3909233",
+    "name": "Oruro"
+  },
+  {
+    "id": "3903319",
+    "name": "Tarija"
+  },
+  {
+    "id": "3923172",
+    "name": "Beni"
+  },
+  {
+    "id": "3908600",
+    "name": "Pando"
+  },
+  {
+    "id": "3907580",
+    "name": "Potosí"
+  }
+]
+
 const API_KEY = '284aad3b1f0e19c9948e86c8752e4e71'
+
+const mainElement = document.getElementById('weather-app-main')
+const themeToggle = document.getElementById('theme-toggle-input')
+const themeLabel = document.getElementById('theme-label')
+const footer = document.getElementsByTagName('footer')[0]
+const citiesDropdown = document.getElementById('cities-dropdown')
 
 setInterval(() => {
   document.getElementById('clock').innerHTML = new Date().toLocaleTimeString()
 }, 1000)
 
+cities.forEach(city => {
+  const option = document.createElement('option')
+  option.appendChild(document.createTextNode(city.name))
+  citiesDropdown.appendChild(option)
+})
+
+citiesDropdown.addEventListener('change', e => {
+  getData()
+})
+
 const getData = async () => {
-  const tempData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Cochabamba&units=metric&appid=${API_KEY}`)
+  const city = document.getElementById('cities-dropdown').value
+  const tempData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
   const temp = await tempData.json()
   console.log(temp);
   document.getElementById('temperature').innerText = parseInt(temp.main.temp)
@@ -18,16 +74,10 @@ const getData = async () => {
   document.getElementsByTagName('img')[0].src = `https://openweathermap.org/img/wn/${temp.weather[0].icon}@4x.png`
 }
 
-const fetchedData = getData()
-console.log(fetchedData)
-
-const mainElement = document.getElementById('weather-app-main')
-const themeToggle = document.getElementById('theme-toggle-input')
-const themeLabel = document.getElementById('theme-label')
-const footer = document.getElementsByTagName('footer')[0]
-
-themeToggle.addEventListener('change', (e) => {
+themeToggle.addEventListener('change', e => {
   themeLabel.innerText = e.target.checked ? 'Dark' : 'Light'
   mainElement.className = `${e.target.checked ? 'dark' : 'light'}-theme`
   footer.className = `${e.target.checked ? 'dark' : 'light'}-theme`
 })
+
+getData()
